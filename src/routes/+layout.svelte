275d06b260a389464edcd4f7d81cfaf8b73435d6
Ravi <r2a3v5i7.kumar@gmail.com>
2025-08-04 16:37:25 -0700

    colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,25 @@
 {@render children?.()}
 
 <style>
+    :root {
+        --text: #0f0321;
+        --subtext: #0f0321;
+        --background: #f8f3fe;
+        --primary: #a675ef;
+        --secondary: #cdabf6;
+        --accent: #edcb57;
+        --linearPrimarySecondary: linear-gradient(#a675ef, #cdabf6);
+        --linearPrimaryAccent: linear-gradient(#a675ef, #edcb57);
+        --linearSecondaryAccent: linear-gradient(#cdabf6, #edcb57);
+        --radialPrimarySecondary: radial-gradient(#a675ef, #cdabf6);
+        --radialPrimaryAccent: radial-gradient(#a675ef, #edcb57);
+        --radialSecondaryAccent: radial-gradient(#cdabf6, #edcb57);
+
+    }
 	:global(body) {
-		width: 100vh;
-		height: 100vw;
+		width: 100vw;
+		height: 100vh;
 		margin: 0;
+		padding: 0;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	<h1>Hello</h1>
 	<p>I make stuff on the internet that nobody asked for</p>
 </div>
+<div class="info-container">
+	<p>Basically I build whatever I want</p>
+	<p>It's usually useless</p>
+</div>
 
 <style>
 	.title-container {
@@ -12,15 +16,26 @@
 		justify-content: center;
 		align-items: center;
 		text-align: center;
-		background-color: #f0f0f0;
+		background-color: var(--secondary);
 	}
-	.title-container h1 {
+	.info-container {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		background-color: var(--accent);
+	}
+	h1 {
 		font-size: 5rem;
 		margin: 0;
+		color: var(--text)
 	}
 
-	.title-container p {
+	p {
 		font-size: 1.5rem;
-		color: #666;
+		color: var(--subtext)
 	}
 </style>
