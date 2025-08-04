e2ee4009cea3a441fb174b38a3a279fa8aa6ad9a
Ravi <r2a3v5i7.kumar@gmail.com>
2025-08-04 12:43:00 -0700

    try to get pages to work?

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    })
+  }
 };
 
 export default config;
