9423e2b75e3f8fa3f5f1ac42ca2e76cc45447013
Ravi <r2a3v5i7.kumar@gmail.com>
2025-08-04 12:53:54 -0700

    build test 2

---
+++ build/_app/immutable/chunks/DR15g06m.js
@@ -0,0 +1,1 @@
+import{e}from"./BnLe6Z7W.js";e();
---
+++ build/_app/immutable/nodes/2.DXO917dU.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/DR15g06m.js";import{f as a,a as p}from"../chunks/BnLe6Z7W.js";var t=a("<h1>Hello</h1>");function f(o){var r=t();p(o,r)}export{f as component};
