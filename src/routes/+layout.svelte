d1bfa16015ddcabbd8fded4bd0134db20aa121f6
Ravi <r2a3v5i7.kumar@gmail.com>
2025-08-05 15:44:06 -0700

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,21 +10,20 @@
 {@render children?.()}
 
 <style>
-    :root {
-        --text: #0f0321;
-        --subtext: #0f0321;
-        --background: #f8f3fe;
-        --primary: #a675ef;
-        --secondary: #cdabf6;
-        --accent: #edcb57;
-        --linearPrimarySecondary: linear-gradient(#a675ef, #cdabf6);
-        --linearPrimaryAccent: linear-gradient(#a675ef, #edcb57);
-        --linearSecondaryAccent: linear-gradient(#cdabf6, #edcb57);
-        --radialPrimarySecondary: radial-gradient(#a675ef, #cdabf6);
-        --radialPrimaryAccent: radial-gradient(#a675ef, #edcb57);
-        --radialSecondaryAccent: radial-gradient(#cdabf6, #edcb57);
-
-    }
+	:root {
+		--text: #0f0321;
+		--subtext: #0f0321;
+		--background: #f8f3fe;
+		--primary: #a675ef;
+		--secondary: #cdabf6;
+		--accent: #edcb57;
+		--linearPrimarySecondary: linear-gradient(#a675ef, #cdabf6);
+		--linearPrimaryAccent: linear-gradient(#a675ef, #edcb57);
+		--linearSecondaryAccent: linear-gradient(#cdabf6, #edcb57);
+		--radialPrimarySecondary: radial-gradient(#a675ef, #cdabf6);
+		--radialPrimaryAccent: radial-gradient(#a675ef, #edcb57);
+		--radialSecondaryAccent: radial-gradient(#cdabf6, #edcb57);
+	}
 	:global(body) {
 		width: 100vw;
 		height: 100vh;
